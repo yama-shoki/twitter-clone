7404906d8a4261f70beeda0a8df070e266248387
shoki-yamamoto <yamamoto@wonq.co.jp>
2025-05-07 22:04:55 +0900

    修正

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
-  api_key: process.env.CLOUDINARY_API_KEY || "",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "",
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || "",
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY || "",
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET || "",
 });
 
 export async function uploadImage(file: string): Promise<string> {
